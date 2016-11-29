51934160b4c962a2ec5ddde4cc155c1721872b52
Eslam Samir <eslamsamir232@gmail.com>
2016-11-29 23:09:33 +0200

    display current position and place marker on click

--- server/map/static/map/js/map.js
+++ server/map/static/map/js/map.js
@@ -1,24 +1,66 @@
 /**
  * Created by Eslam on 2016-11-27.
  */
+var marker, geoMarker;
+var geoCircle;
+var currentPos;
+var map;
+
 function initMap() {
-    var map = new google.maps.Map(document.getElementById('map'), {
+    map = new google.maps.Map(document.getElementById('map'), {
         center: {lat: -34.397, lng: 150.644}, // TODO set default map center
         zoom: 15
     });
-    var infoWindow = new google.maps.InfoWindow({map: map});
 
+    navigateToCurrentLocation();
+
+    map.addListener("click", function (event) {
+        addMarker(event.latLng, map);
+    });
+}
+
+// center the map at the current location
+function navigateToCurrentLocation() {
+    var infoWindow = new google.maps.InfoWindow({map: map});
     // Try HTML5 geolocation.
     if (navigator.geolocation) {
-        navigator.geolocation.getCurrentPosition(function (position) {
-            var pos = {
-                lat: position.coords.latitude,
-                lng: position.coords.longitude
-            };
+        navigator.geolocation.watchPosition(function (position) {
+            currentPos = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
+            map.setCenter(currentPos);
 
-            infoWindow.setPosition(pos);
-            infoWindow.setContent('Location found.');
-            map.setCenter(pos);
+            if (geoCircle != null)
+            {
+                geoCircle.setMap(null);
+            }
+            if (geoMarker != null) {
+                geoMarker.setMap(null);
+            }
+            geoCircle = new google.maps.Circle({
+                center: currentPos,
+                radius: position.coords.accuracy,
+                map: map,
+                fillColor: '#0000FF',
+                fillOpacity: 0.2,
+                strokeColor: '#0000FF',
+                strokeOpacity: 0.5,
+                strokeWeight: 0.8
+            });
+            geoCircle.setMap(map);
+            geoMarker = new google.maps.Marker({
+                position: currentPos,
+                icon: {
+                    path: google.maps.SymbolPath.CIRCLE,
+                    fillColor: '#0000FF',
+                    fillOpacity: 0.5,
+                    strokeWeight: 0,
+                    scale: 8
+                },
+                map: map,
+                title: 'Current Location'
+            });
+            geoMarker.setMap(map);
+            console.log('Source: ' + currentPos.lat() + ', ' + currentPos.lng());
+
         }, function () {
             handleLocationError(true, infoWindow, map.getCenter());
         });
@@ -26,6 +68,21 @@ function initMap() {
         // Browser doesn't support Geolocation
         handleLocationError(false, infoWindow, map.getCenter());
     }
+}
+
+// adds destination marker on click event
+function addMarker(location) {
+    if (marker != null)
+    {
+        marker.setMap(null);
+    }
+    marker = new google.maps.Marker({
+        position: location,
+        map: map,
+        title: 'Destination'
+    });
+    marker.setMap(map);
+    console.log('Destination: ' + location.lat() + ', ' + location.lng());
 }
 
 function handleLocationError(browserHasGeolocation, infoWindow, pos) {

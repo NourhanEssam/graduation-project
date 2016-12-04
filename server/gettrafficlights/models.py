1fd5ba4b9f678635bbf79f97e515628688f7a689
Yasminemibrahim <yasmeen.m.ibrahim@gmail.com>
2016-12-04 19:22:10 +0200

    Modified keys

--- server/gettrafficlights/models.py
+++ server/gettrafficlights/models.py
@@ -1,42 +1,45 @@
 from django.db import models
 
 # Create your models here.
-from django.db import models
 
-# Create your models here.
+
 class Intersection(models.Model):
-    Intersection_ID=models.CharField(max_length=4)
+    Intersection_ID = models.CharField(max_length=10, primary_key=True)
 
-class TrafficLight( models.Model):
-    TL_ID=models.CharField(max_length=5)
-    Longitude=models.CharField(max_length=25)
-    Latitude=models.CharField(max_length=25)
-    Direction=models.CharField(max_length=2)
-    Intersection_ID=models.ForeignKey(Intersection,on_delete=models.CASCADE)
-class Street( models.Model):
-    Street_ID=models.CharField(max_length=5)
-    Start_Longitude=models.CharField(max_length=25)
-    Start_Latitude=models.CharField(max_length=25)
-    End_Longitude = models.CharField(max_length=25)
-    End_Latitude = models.CharField(max_length=25)
 
-class Driver( models.Model):
-    National_ID=models.CharField(max_length=5)
-    First_Name=models.CharField(max_length=250)
-    Last_Name=models.CharField(max_length=250)
-    Username=models.CharField(max_length=25)
-    Encrypted_Password=models.CharField(max_length=25)
+class TrafficLight(models.Model):
+    TL_ID = models.CharField(max_length=10, primary_key=True)
+    Longitude = models.FloatField(max_length=25)
+    Latitude = models.FloatField(max_length=25)
+    Direction = models.CharField(max_length=2)
+    Intersection_ID = models.ForeignKey(Intersection, on_delete=models.CASCADE)
+
+
+class Street(models.Model):
+    Street_ID = models.CharField(max_length=10, primary_key=True)
+    Start_Longitude = models.FloatField(max_length=25)
+    Start_Latitude = models.FloatField(max_length=25)
+    End_Longitude = models.FloatField(max_length=25)
+    End_Latitude = models.FloatField(max_length=25)
+    Intersection_ID = models.ManyToManyField(Intersection)
+
+
+class Driver(models.Model):
+    National_ID = models.CharField(max_length=20, primary_key=True)
+    First_Name = models.CharField(max_length=250)
+    Last_Name = models.CharField(max_length=250)
+    Username = models.CharField(max_length=25)
+    Encrypted_Password = models.CharField(max_length=15)
+
 
 class EmergencyVehicle(models.Model):
-    Car_ID=models.CharField(max_length=10)
-    Type=models.CharField(max_length=20)
+    Car_ID = models.CharField(max_length=10, primary_key=True)
+    Type = models.CharField(max_length=20)
 
-class Contains(models.Model):
-    Intersection_ID=models.ForeignKey(Intersection, on_delete=models.CASCADE)
-    Street_ID=models.ForeignKey(Street, on_delete=models.CASCADE)
 
 class Use(models.Model):
-    National_ID=models.ForeignKey(Driver, on_delete=models.CASCADE)
-    Car_ID=models.ForeignKey(EmergencyVehicle, on_delete=models.CASCADE)
-    Date=models.DateTimeField
+    unique_together = ('National_ID', 'Car_ID')
+    National_ID = models.ForeignKey(Driver, on_delete=models.CASCADE)
+    Car_ID = models.ForeignKey(EmergencyVehicle, on_delete=models.CASCADE)
+    Date = models.DateTimeField
 

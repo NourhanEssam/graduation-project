4f78919284b61f5a4b4d9389897970757d50868c
NourhanEssam <nourhanessam94@gmail.com>
2016-12-13 23:16:09 +0200

    transfering apps to views

--- server/gettrafficlights/views.py
+++ server/gettrafficlights/views.py
@@ -1,13 +1,36 @@
-from django.shortcuts import render
-
-# Create your views here.
 from django.http import HttpResponse
+from .models import TrafficLight
 
 
 def index(request):
     return HttpResponse("Hello, world. You're at the gettrafficlights index.")
 
 
-from django.shortcuts import render
+def file_len(fname):
+    j=0
+    with open(fname) as f:
+        for j, l in enumerate(f):
+            pass
+    return j + 1
+
+
+def gettl():
+
+    lines = open('myfile.txt').readlines()
+    l = file_len('myfile.txt')
+
+    i = 0
+    while i < l:
+        street1 = lines[i]
+        dir = lines[i+1]
+        street2 = lines[i+2]
+        tls = []
+        i += 2
+        tls[i] = TrafficLight.objects.raw('SELECT TL_ID FROM TrafficLight WHERE Direction=dir AND Intersection_ID in SELECT A.Intersection_ID FROM IntersectionsStreets A B WHERE A.Street_ID=street1 AND B.Street_ID=street2 AND A.Intersection_ID=B.Intersection_ID')
+        print(tls[i])
+    open('myfile.txt', 'w').writelines(tls)
+
+gettl()
+
 
 # Create your views here.

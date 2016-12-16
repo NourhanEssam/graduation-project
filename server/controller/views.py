73b91c40ae1fee5a4499cda439223e9f9157e032
NourhanEssam <nourhanessam94@gmail.com>
2016-12-16 22:38:41 +0200

    Clearing controller

--- server/controller/views.py
+++ server/controller/views.py
@@ -8,16 +8,4 @@ from server.settings import INSTALLED_APPS
 
 
 def index(request):
-    # List of TLs (Hard Coded)
-    tls = ['100N', '2S', '3E', '5W', '6N']
-
-    # Get List of Directions and Intersections
-    for tl in tls:
-        Queryset = TrafficLight.objects.filter(Intersection_ID=str(re.split('(\d+)', tl)[1]))
-        for element in Queryset:
-            if element.Direction == re.split('(\d+)', tl)[2]:
-                # open socket for ON
-            else:
-                # open socket for OFF
-
     return HttpResponse("<h1>Hello Controller</h1>");

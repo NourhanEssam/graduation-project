1d17df44d5b8b35db76112670de0676cbfe8637e
NourhanEssam <nourhanessam94@gmail.com>
2016-12-20 21:06:36 +0200

    #7 - editing controller views

--- server/controller/views.py
+++ server/controller/views.py
@@ -1,50 +1,50 @@
+from controller.models import TrafficLight
+from server.settings import INSTALLED_APPS
 from django.shortcuts import render
 from django.http import HttpResponse
-from controller.models import TrafficLight
 import re
 import socket
 
-# Create your views here.
-from server.settings import INSTALLED_APPS
-
-
 def index(request):
-    # List of TLs (Hard Coded)
-    tls = ['100N', '2S', '3E', '5W', '6N']
+    tls = ['1N', '2W', '3E', '5W', '6N']
 
-    # Open Socket for the server
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    s.bind((socket.gethostname(), 12345))
+    s.bind(1234)
     s.listen(1)
-    # Accept the EV
     EVconnection, EVaddress = s.accept()
 
-    data = EVconnection.recv(20)
-    if data == "1":  # Ready to receive
-        # Get List of Directions and Intersections
+    if EVconnection.recv(20) == '1':  # EV is Ready
         for tl in tls:
             Queryset = TrafficLight.objects.filter(Intersection_ID=str(re.split('(\d+)', tl)[1]))
             for element in Queryset:
-                tl_ip = TrafficLight.objects.values('TL_ID').filter(Intersection_ID=str(re.split('(\d+)', tl)[1])).filter(Direction=str(re.split('(\d+)', tl)[2])) # Get IP
+                tl_ip = TrafficLight.objects.values('TL_ID').filter(Intersection_ID=str(re.split('(\d+)', tl)[1])).filter(Direction=str(re.split('(\d+)', tl)[2]))
                 tl_port = 12345
                 tlsocket = socket(AF_INET, SOCK_STREAM)
-                tlsocket.connect((tl_ip, tl_port))
-                if element.Direction == re.split('(\d+)', tl)[2]:       # ON
-                    tlsocket.send("1")
-                    #data = tlsocket.recv(20)
+                tlsocket.connect(tl_ip, tl_port)
 
-                else:   # OFF
-                    tlsocket.send("0")
-                    #data = tlsocket.recv(20)
+                if tlsocket.recv(20) == '1':  # TL confirms the connection
+                    if element.Direction == re.split('(\d+)', tl)[2]:
+                     tlsocket.send('1+element.Direction')
+                    else:
+                     tlsocket.send('0+element.Direction')
 
-                # Receive from EV
-                data = EVconnection.recv(20)
-                if data == "1":
-                    #reset TLs
-                    #close sockets
-                    tlsocket.close()
-        # if end of route break
-        data = EVconnection.recv(20)
-        if data == "1": #end of route
-            EVconnection.close()
+                if EVconnection.recv(20) == '2':
+                     tlsocket.send("2")  # reset TLs
+                     tlsocket.close()
+
+    if EVconnection.recv(20) == '3': #end of route
+         EVconnection.close()
     return HttpResponse("<h1>Hello Controller</h1>");
+
+###  Protocol  ###
+    #Emergency Vehicle
+        #Send 0 --> Reconnect
+        #Send 1 --> confirm the connection
+        #Send 2 --> moved
+        #Send 3 --> End of route
+    #Traffic Light
+        #Receive 0,dir -->OFF
+        #Receive 1,dir -->ON
+        #Send 0 --> Reconnect
+        #Send 1 --> confirm the connection
+

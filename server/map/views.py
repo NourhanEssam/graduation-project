63771eda98a3b1265fe5a36ad4c040302a9a6040
Eslam Samir <eslamsamir232@gmail.com>
2017-01-23 19:17:01 +0200

    fixes

--- server/map/views.py
+++ server/map/views.py
@@ -1,16 +1,31 @@
 from django.views.decorators.csrf import ensure_csrf_cookie
 from django.shortcuts import render
 from googlemaps import *
+import parser
+import urllib
 
 @ensure_csrf_cookie
 def index(request):
     if request.method == 'POST' and request.is_ajax():
         data = request.POST
-        gmaps = Client(key='AIzaSyCwI32R7w5UyY3nvL8Pv-sJqBvUiGCTzOc')
-        directions_result = gmaps.directions((data.get('currentPosLat'), data.get('currentPosLng')),
-                                             (data.get('targetLat'), data.get('targetLng')),
-                                             mode="driving")
-        print directions_result
+
+        directions_url = 'https://maps.googleapis.com/maps/api/directions/json?sensor=false&origin={},{}&destination={},{}&key={}&mode={}'
+        originLat = float(data.get('currentPosLat'))
+        originLong = float(data.get('currentPosLng'))
+        destinationLat = float(data.get('targetLat'))
+        destinationLong = float(data.get('targetLng'))
+        key = "AIzaSyCwI32R7w5UyY3nvL8Pv-sJqBvUiGCTzOc"
+        mode = "driving"
+        pars = (originLat, originLong, destinationLat, destinationLong, key, mode)
+        request_url = directions_url.format(*pars)
+        result_string = urllib.urlopen(request_url).read()
+
+        directions = parser.parse_directions(result_string)
+
+        # for testing
+        print request_url, "\n\n"
+        print result_string, "\n\n"
+        print directions, "\n\n"
     return render(request, 'map/index.html')
 
 

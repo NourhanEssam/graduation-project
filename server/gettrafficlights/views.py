0421488efd600e86af49f3aa2ad872a4ebb7664f
Eslam Samir <eslamsamir232@gmail.com>
2017-01-25 21:56:00 +0200

    #6 - integrated map with gettrafficlights

--- server/gettrafficlights/views.py
+++ server/gettrafficlights/views.py
@@ -1,45 +1,34 @@
-from django.http import HttpResponse
 from .models import TrafficLight
 from .models import IntersectionsStreets
-from django.conf import settings
-from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
+from django.shortcuts import render, redirect
 
 
+@login_required
 def index(request):
-    path = settings.GETTRAFFICLIGHTS_FOLDER+'myfile.txt'
-    lines = open(path, 'r').read().replace('\n', '')
-    l = file_len(path)
-    i = 0
-    tls = []
-    print path
-    print l
-    while i < l-2:
-        street1 = lines[i]
-        direct = lines[i + 1]
-        street2 = lines[i + 2]
+    if request.user.is_superuser or request.user.is_staff:
+        return redirect('login:logout')
 
-        Queryset = IntersectionsStreets.objects.filter(Street_ID=street1).values('Intersection_ID')
-        Queryset2 = IntersectionsStreets.objects.filter(Street_ID=street2, Intersection_ID__in=Queryset).values('Intersection_ID')
-        Queryset3 = TrafficLight.objects.filter(Intersection_ID=Queryset2, Direction=direct).values('TL_ID')
-        for element in Queryset3:
-            tls.append(element['TL_ID'])
-        open(path, 'a').writelines(element['TL_ID'])
-        i += 2
-    return render(request, 'map/index.html')
-   # return HttpResponse("Hello, world. You're at the gettrafficlights index.")
+    if 'directions' in request.session:
+        lines = request.session['directions']
+        tls = []
+        print lines
+        i = 0
+        while i < len(lines)-2:
+            street1 = lines[i]
+            direct = lines[i + 1]
+            street2 = lines[i + 2]
 
+            Queryset = IntersectionsStreets.objects.filter(Street_ID=street1).values('Intersection_ID')
+            Queryset2 = IntersectionsStreets.objects.filter(Street_ID=street2, Intersection_ID__in=Queryset).values('Intersection_ID')
+            Queryset3 = TrafficLight.objects.filter(Intersection_ID=Queryset2, Direction=direct).values('TL_ID')
+            for element in Queryset3:
+                tls.append(element['TL_ID'])
+            i += 2
 
-def file_len(fname):
-    j = 0
-    with open(fname) as f:
-        for j, l in enumerate(f):
-            pass
-    return j + 1
+        print tls
+        return redirect('map:map')
+
+    return redirect('map:map')
 
 
-
-
-
-
-
-# Create your views here.

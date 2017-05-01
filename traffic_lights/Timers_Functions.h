ca3ac4cc10c979f0c514d626af372a0833198883
NourhanEssam <nourhanessam94@gmail.com>
2017-05-01 13:08:06 +0200

    TL25 - Timers (SysTick, 0A, 1A, 2A, 3A) added

---
+++ traffic_lights/Timers_Functions.h
@@ -0,0 +1,7 @@
+void SysTick_Init(void);
+void SysTick_Wait(unsigned long delay);
+void SysTick_delay10MS(unsigned long delay);
+void timer0A_delayMs(int ttime);
+void timer1A_delayMs(int ttime);
+void timer2A_delayMs(int ttime);
+void timer3A_delayMs(int ttime);

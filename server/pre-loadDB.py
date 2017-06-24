54b5ed23592f0046615ff3b3627daaed32dfcece
NourhanEssam <nourhanessam94@gmail.com>
2017-06-24 14:31:27 +0200

    #32 - updating pre-load DB script

--- server/pre-loadDB.py
+++ server/pre-loadDB.py
@@ -1,4 +1,14 @@
 import subprocess
+import os
+import shutil
+
+if os.path.isfile("./db.sqlite3"):
+	os.remove("./db.sqlite3")
+	
+if os.path.isdir("./db.sqlite3"):	
+	shutil.rmtree("./gettrafficlights/migrations")
+	
+subprocess.Popen(["python", "manage.py", "makemigrations", "gettrafficlights" ]).communicate()
 subprocess.Popen(["python", "manage.py", "makemigrations", "--empty", "gettrafficlights" ]).communicate()
 subprocess.Popen(["python", "manage.py", "migrate" ]).communicate()
-subprocess.Popen(["python", "manage.py", "loaddata", "./gettrafficlights/fixtures/data.json" ]).communicate()
+subprocess.Popen(["python", "manage.py", "loaddata", "./gettrafficlights/fixtures/database.json" ]).communicate()

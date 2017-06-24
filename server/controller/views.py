fe0798c0799c69d5c434c765437eb167d6169403
NourhanEssam <nourhanessam94@gmail.com>
2017-06-24 17:12:12 +0200

    using passed_tl

--- server/controller/views.py
+++ server/controller/views.py
@@ -2,6 +2,7 @@ from gettrafficlights.models import TrafficLight, CentralNode, Intersection
 
 import re
 import socket
+import utilities
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render, redirect
 
@@ -11,52 +12,50 @@ def index(request):
     if request.user.is_superuser or request.user.is_staff:
         return redirect('login:logout')
 
-    if request.method == 'POST' and request.is_ajax():
-        data = request.POST
-        print "hello data\n"
-        print data
-
-        # TODO use passed_tl function
-
     if 'TrafficLights' in request.session:
         TLs_IDs = request.session['TrafficLights']
 
     #Testing
     TLs_IDs = [1,2]
+    Passed = False
 
     for tl_id in TLs_IDs:
-        Queryset = TrafficLight.objects.filter(TL_ID=tl_id).values('Intersection_ID','Direction').first()
+        # Get intersection and direction
+        Queryset = TrafficLight.objects.filter(TL_ID=tl_id).values('Intersection_ID', 'Direction').first()
         TL_Intersection_ID = Queryset['Intersection_ID']
         TL_Direction = Queryset['Direction']
 
+        # Get TL location from database
+        TLLocation = TrafficLight.objects.filter(TL_ID=tl_id).values('Longitude', 'Latitude').first()
+        TLLocation['lon'] = TLLocation.pop('Longitude')
+        TLLocation['lat'] = TLLocation.pop('Latitude')
+
+        # Get Central node Intersection number
         Intersection_Number_Central_Node = Intersection.objects.values('Intersection_Number_Central_Node').filter(Intersection_ID=TL_Intersection_ID).first()['Intersection_Number_Central_Node']
+
+        # Get ip and port number
         tl_ip = CentralNode.objects.values('CentralNode_IP').filter(CentralNode_ID__in=Intersection.objects.values('CentralNode_ID').filter(Intersection_ID=TL_Intersection_ID)).first()['CentralNode_IP']
         tl_port = 12348
-
         #Testing
         tl_ip = 'LAPTOP-S9D924P3'
 
-        #tlsocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        #tlsocket.connect((tl_ip, tl_port))
-
+        tlsocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        tlsocket.connect((tl_ip, tl_port))
+        tlsocket.send(Intersection_Number_Central_Node + TL_Direction)
         print 'Sending ', Intersection_Number_Central_Node, TL_Direction
 
-        #tlsocket.send(Intersection_Number_Central_Node + TL_Direction)
+        while Passed == False:
+            if request.method == 'POST' and request.is_ajax():
+                data = request.POST
+                EVlocation = {}
+                EVlocation['lon'] = data['currentPosLng']
+                EVlocation['lat'] = data['currentPosLat']
+                EVlocation = {key: float(value) for (key, value) in EVlocation.items()}
+                Passed = utilities.passed_tl(EVlocation, TLLocation, TL_Direction)
+            print Passed
 
-        #if EVconnection.recv(20) == '2':
-        #    tlsocket.send("2")  # reset TLs
-        #    tlsocket.close()
+        # reset TLs
+        tlsocket.send("P")
+        tlsocket.close()
 
     return redirect('map:map')
-
-    ###  Protocol  ###
-    # Emergency Vehicle
-    # Send 0 --> Reconnect
-    # Send 1 --> confirm the connection
-    # Send 2 --> moved
-    # Send 3 --> End of route
-    # Traffic Light
-    # Receive 0,dir -->OFF
-    # Receive 1,dir -->ON
-    # Send 0 --> Reconnect
-    # Send 1 --> confirm the connection

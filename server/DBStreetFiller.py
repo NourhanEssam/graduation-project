8bd289e64dfa08c7a4e1948d201052dbcbe46fd6
Michel Mina <michel.zekry@gmail.com>
2017-06-26 16:21:59 +0200

    Update DBStreetFiller.py

--- server/DBStreetFiller.py
+++ server/DBStreetFiller.py
@@ -1,17 +1,22 @@
-import urllib2
-import xmltodict
-import time
+import urllib2,xmltodict,os
 
-start_time = time.time()
 street_list = []
 Kolleya_Origin_lat=30.064410
 Kolleya_Origin_lng=31.278965
+
+def Write_model(StreetName,StartLng,StartLat,EndLng,EndLat):
+    outputfile.write("\n  {\n    \"model\": \"gettrafficlights.street\",\n    \"pk\": \"")
+    outputfile.write(StreetName+"\",\n    \"fields\":\n    {\n      \"Start_Longitude\": ")
+    outputfile.write(StartLng+",\n      \"Start_Latitude\": ")
+    outputfile.write(StartLat+",\n      \"End_Longitude\": ")
+    outputfile.write(EndLng+  ",\n      \"End_Latitude\": ")
+    outputfile.write(EndLat+"\n    }\n  },")
 
 def Get_Streets(lat=30.064410,lng=31.278965,username="micheltls"):
 
     service_url = "http://api.geonames.org/findNearbyStreetsOSM?lat=" + str(lat) +\
                   "&lng=" + str(lng) + "&username=" + username + "&radius=1"
-
+    print "Getting all street names ..."
     response = urllib2.urlopen(service_url)
     xml_response = response.read()
     i=1
@@ -22,53 +27,69 @@ def Get_Streets(lat=30.064410,lng=31.278965,username="micheltls"):
             i +=1
             lng=coords.split()[0]
             lat=coords.split()[1]
-            reverse_url = "https://maps.googleapis.com/maps/api/geocode/xml?&latlng="+str(lat)+","+str(lng)
+            reverse_url = "https://maps.googleapis.com/maps/api/geocode/xml?&latlng="+str(lat)+","+str(lng)+ \
+                "&key=AIzaSyCjYtjT-xGAZ-sdjDeQTAMULD_deVuW20k"
             try:
+                print "Reversing ..."
                 reverse_response = urllib2.urlopen(reverse_url)
+                print "Reversed"
             except urllib2.URLError:
                 continue
             xml_response = xmltodict.parse(reverse_response.read())
             try :
                 street_name = xml_response['GeocodeResponse']['result'][0]['formatted_address'].split(",")[0]
-                while street_name[0].isdigit():
-                    street_name = street_name[street_name.find(" ")+1:]
-                if (street_list.count(street_name) == 0 ):
-                    street_list.append(street_name)
-                    print street_name
-
             except KeyError:
                 print xml_response['GeocodeResponse']
                 continue
+            while street_name[0].isdigit():
+                street_name = street_name[street_name.find(" ")+1:]
+            if (street_list.count(street_name) == 0 ):
+                street_list.append(street_name)
+                geocode_service_url="https://maps.googleapis.com/maps/api/geocode/xml?address="+\
+                    xml_response['GeocodeResponse']['result'][0]['formatted_address'].replace(" ","%20")+\
+                    "&key=AIzaSyCjYtjT-xGAZ-sdjDeQTAMULD_deVuW20k"
+                print "Geocoding ..."
+                try:
+                    geocode_response = urllib2.urlopen(geocode_service_url).read()
+                except urllib2.URLError:
+                    print "ERR Geocoding"
+                print "Geocoded"
+                try:
+                    bounds =  xmltodict.parse(geocode_response)['GeocodeResponse']['result']['geometry']['bounds']
+                except TypeError:
+                    print xmltodict.parse(geocode_response)['GeocodeResponse']['result']
+                except KeyError:
+                    print xmltodict.parse(geocode_response)['GeocodeResponse']['result']
+                start = bounds['northeast']
+                end =   bounds['southwest']
+                Write_model(street_name,start['lng'],start['lat'],end['lng'],end['lat'])
+            else:
+                print "Again?"
 
 
-    '''
-    #example = dict_response['geonames']['streetSegment'][0]['line'].split(",")[0].split()
-    print(example)
-    print service_url
 
-    reverse_url = "https://maps.googleapis.com/maps/api/geocode/xml?&latlng="+str(example[1])+","+str(example[0])
-    reverse_response = urllib2.urlopen(reverse_url)
 
-    xml_response = xmltodict.parse(reverse_response.read())
-    print reverse_url
-    print xml_response['GeocodeResponse']['result'][0]['formatted_address']
-    '''
+outputfile = open("gettrafficlights/fixtures/data2.json",mode='w')
+outputfile.write("[")
+
 Get_Streets(lat=Kolleya_Origin_lat,lng=Kolleya_Origin_lng)
-print 1
+print 1*1000
 Get_Streets(lat=Kolleya_Origin_lat+0.005,lng=Kolleya_Origin_lng+0.005)
-print 2
+print 2*1000
 Get_Streets(lat=Kolleya_Origin_lat+0.005,lng=Kolleya_Origin_lng)
-print 3
+print 3*1000
 Get_Streets(lat=Kolleya_Origin_lat,lng=Kolleya_Origin_lng+0.005)
-print 4
+print 4*1000
 Get_Streets(lat=Kolleya_Origin_lat-0.005,lng=Kolleya_Origin_lng)
-print 5
+print 5*1000
 Get_Streets(lat=Kolleya_Origin_lat,lng=Kolleya_Origin_lng-0.005)
-print 6
+print 6*1000
 Get_Streets(lat=Kolleya_Origin_lat-0.005,lng=Kolleya_Origin_lng+0.005)
-print 7
+print 7*1000
 Get_Streets(lat=Kolleya_Origin_lat+0.005,lng=Kolleya_Origin_lng-0.005)
-print 8
-for i in street_list:
-    print i
-print("--- %s seconds ---" % (time.time() - start_time))
+print 8*1000
+
+
+outputfile.seek(-1,os.SEEK_CUR)
+outputfile.write("\n]")
+outputfile.close()

ae495dafd7fe2f68440cb32c936500b0edf7e12e
Eslam Samir <eslamsamir232@gmail.com>
2017-06-30 21:40:11 +0200

    #34 - Map Searchbar

--- server/map/static/map/js/map.js
+++ server/map/static/map/js/map.js
@@ -8,6 +8,8 @@ var geoCircle;
 var currentPos;
 var map;
 
+var searchMarker;
+
 var timer = null;
 
 // initial function called by the google maps api
@@ -19,7 +21,70 @@ function initMap() {
 
     navigateToCurrentLocation();
 
+    var card = document.getElementById('pac-card');
+    var input = document.getElementById('pac-input');
+
+    map.controls[google.maps.ControlPosition.TOP_RIGHT].push(card);
+
+    var autocomplete = new google.maps.places.Autocomplete(input);
+
+    // Bind the map's bounds (viewport) property to the autocomplete object,
+    // so that the autocomplete requests use the current map bounds for the
+    // bounds option in the request.
+    autocomplete.bindTo('bounds', map);
+
+    // Autocomplete types
+    autocomplete.setTypes([]);
+
+    var infowindow = new google.maps.InfoWindow();
+    var infowindowContent = document.getElementById('infowindow-content');
+    infowindow.setContent(infowindowContent);
+
+    // place marker
+    searchMarker = new google.maps.Marker({
+        map: map,
+        anchorPoint: new google.maps.Point(0, -29)
+    });
+
+    autocomplete.addListener('place_changed', function () {
+        infowindow.close();
+        searchMarker.setVisible(false);
+        var place = autocomplete.getPlace();
+        if (!place.geometry) {
+            // User entered the name of a Place that was not suggested and
+            // pressed the Enter key, or the Place Details request failed.
+            window.alert("No details available for input: '" + place.name + "'");
+            return;
+        }
+
+        // If the place has a geometry, then present it on a map.
+        if (place.geometry.viewport) {
+            map.fitBounds(place.geometry.viewport);
+        } else {
+            map.setCenter(place.geometry.location);
+            map.setZoom(15);
+        }
+        searchMarker.setPosition(place.geometry.location);
+        searchMarker.setVisible(true);
+
+        var address = '';
+        if (place.address_components) {
+            address = [
+                (place.address_components[0] && place.address_components[0].short_name || ''),
+                (place.address_components[1] && place.address_components[1].short_name || ''),
+                (place.address_components[2] && place.address_components[2].short_name || '')
+            ].join(' ');
+        }
+
+        infowindowContent.children['place-icon'].src = place.icon;
+        infowindowContent.children['place-name'].textContent = place.name;
+        infowindowContent.children['place-address'].textContent = address;
+        infowindow.open(map, searchMarker);
+    });
+
+    // map onclick event handler
     map.addListener("click", function (event) {
+        searchMarker.setVisible(false);
         calcRoute(event.latLng);
     });
 }

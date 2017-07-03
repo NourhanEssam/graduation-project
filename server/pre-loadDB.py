4a96544046fed11928c198f33ba807790ce64f8a
NourhanEssam <nourhanessam94@gmail.com>
2017-07-03 21:06:25 +0200

    pre-load DB updated

--- server/pre-loadDB.py
+++ server/pre-loadDB.py
@@ -6,9 +6,18 @@ if os.path.isfile("./db.sqlite3"):
 	os.remove("./db.sqlite3")
 	
 if os.path.isdir("./db.sqlite3"):	
-	shutil.rmtree("./gettrafficlights/migrations")
-	
+	shutil.rmtree("./gettrafficlights/migrations")	
+
 subprocess.Popen(["python", "manage.py", "makemigrations", "gettrafficlights" ]).communicate()
 subprocess.Popen(["python", "manage.py", "makemigrations", "--empty", "gettrafficlights" ]).communicate()
 subprocess.Popen(["python", "manage.py", "migrate" ]).communicate()
 subprocess.Popen(["python", "manage.py", "loaddata", "./gettrafficlights/fixtures/database.json" ]).communicate()
+
+check_user = raw_input("Load Streets in Fixtures? (Y/N) ")
+if check_user == 'Y':
+	subprocess.Popen(["python", ".\DBStreetFiller.py" ]).communicate()
+
+
+check_user = raw_input("Include Extracted Streets in the Database? (Y/N) ")
+if check_user == 'Y':
+	subprocess.Popen(["python", "manage.py", "loaddata", "./gettrafficlights/fixtures/streets.json" ]).communicate()
